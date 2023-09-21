1f1c3e64c25f11b332dcdbcc8bc7cedfbe900fd4
prajjwal ajit <122018880+Prajjwalajit15@users.noreply.github.com>
2023-09-21 17:01:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your JS code here. If required.
+document.addEventListener("DOMContentLoaded", function() {
+ const clr = document.getElementById("colorSelect");
+const btn = document.querySelector("form > input");
+
+
+	btn.addEventListener("click", function() {
+	const selectedClr = clr.options[clr.selectedIndex];
+	if (selectedClr) {
+			clr.removeChild(selectedClr);
+	   }
+	});
+});
